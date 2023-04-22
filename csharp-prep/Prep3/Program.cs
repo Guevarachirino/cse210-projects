b3e81ca1c1933c937eee3171d2ad3eeaa40c81af
Guevarachirino <chi23017@byui.edu>
2023-04-22 08:58:27 -0400

    more

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string userInput = Console.ReadLine();
+        int magicnumber = int.Parse(userInput);
+        Console.Write("What is your guess? ");
+        string userguess = Console.ReadLine();
+        int guessnumber = int.Parse(userguess);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(magicnumber);
+
+       //int guessnumber = -1;
+
+        // We could also use a do-while loop here...
+        while (guessnumber != number)
+        {
+            Console.Write("What is your guess? ");
+            guessnumber = int.Parse(Console.ReadLine());
+
+            if ( guessnumber > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessnumber < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
+
+    
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> newnumber = new List<int>();
+        int number = -1;
+        while (number != 0)
+        {
+            
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                newnumber.Add(number);
+            }
+        }
+            
+
+        }
+
     }
-}
