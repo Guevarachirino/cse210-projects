324513f1e3200c61f34ff5e0f74a879d7480f6c1
Guevarachirino <chi23017@byui.edu>
2023-04-27 23:34:28 -0400

    adding more

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        List<string>   menu = new List<string>
+        {
+            "Please select one of the following choises: ",
+            "1. write",
+            "2. Display",
+            "3. Load",
+            "4. Save",
+            "5. Quit",
+            "What would you like to do?"
+
+        };
+        
     }
 }
